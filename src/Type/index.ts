cc689ce387c5125b2ea410772a553f3205d49a9f
TheByteBits (Mateusz Pietrzak) <mateusz.pietrzak@outlook.com>
2018-11-07 15:48:30 +0100

    ✨ Add an entry import file for the Type module
    Add an index.js file exporting all of the functions of Type module.

---
+++ src/Type/index.ts
@@ -0,0 +1,10 @@
+import isUndefined from "./isUndefined/isUndefined"
+import isNull from "./isNull/isNull"
+import isNill from "./isNill/isNill"
+
+
+export {
+    isUndefined,
+    isNull,
+    isNill
+}

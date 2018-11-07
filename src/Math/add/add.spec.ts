24a38f428bb58a889b286575c28e07ce0049d31e
TheByteBits (Mateusz Pietrzak) <mateusz.pietrzak@outlook.com>
2018-11-07 14:13:59 +0100

    ✨ Add an add function
    Add an add function with unit tests.

---
+++ src/Math/add/add.spec.ts
@@ -0,0 +1,20 @@
+import add from "./add";
+
+
+describe(`Module "Math" -> Function "add"`, () => {
+    it("should preform addition of two numbers.", () => {
+        expect(add(2, 2))
+            .toBe(4)
+
+        expect(add(25, 75))
+            .toBe(100)
+
+        expect(add(75, 25))
+            .toBe(100)
+    })
+
+    it("should be a curried function.", () => {
+        expect(() => add(2)(2))
+            .not.toThrow()
+    })
+})
---
+++ src/Math/add/add.ts
@@ -0,0 +1,8 @@
+import { curry } from "../../Function/index"
+
+
+const add = (a, b) =>
+    a + b;
+
+
+export default curry(add)

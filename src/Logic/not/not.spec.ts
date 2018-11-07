5254aa61b9bd4b3eb873be873c87adcfea559fb1
TheByteBits (Mateusz Pietrzak) <mateusz.pietrzak@outlook.com>
2018-11-07 15:04:04 +0100

    ✨ Add a not function
    Add a not function with unit tests.

---
+++ src/Logic/not/not.spec.ts
@@ -0,0 +1,12 @@
+import not from "./not";
+
+
+describe(`Module "Logic" -> Function "not"`, () => {
+    it("should preform a logical negation of a given boolean value.", () => {
+        expect(not(false))
+            .toBe(true)
+
+        expect(not(true))
+            .toBe(false)
+    })
+})
---
+++ src/Logic/not/not.ts
@@ -0,0 +1,5 @@
+const not = x =>
+    !x
+
+
+export default not

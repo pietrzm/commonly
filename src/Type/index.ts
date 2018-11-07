81392861a14a29b884efd5bacc6f2f665011430d
TheByteBits (Mateusz Pietrzak) <mateusz.pietrzak@outlook.com>
2018-11-07 16:03:36 +0100

    ✨ Update an entry import file for the Type module
    Update an index.js file exporting all of the functions of Type module.

--- src/Type/index.ts
+++ src/Type/index.ts
@@ -1,10 +1,18 @@
 import isUndefined from "./isUndefined/isUndefined"
 import isNull from "./isNull/isNull"
 import isNill from "./isNill/isNill"
+import isBoolean from "./isBoolean/isBoolean"
+import isNumber from "./isNumber/isNumber"
+import isString from "./isString/isString"
+import isSymbol from "./isSymbol/isSymbol"
 
 
 export {
     isUndefined,
     isNull,
-    isNill
+    isNill,
+    isBoolean,
+    isNumber,
+    isString,
+    isSymbol
 }

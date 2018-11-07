c16cb6d4869975e1a48109902ba19631588de556
TheByteBits (Mateusz Pietrzak) <mateusz.pietrzak@outlook.com>
2018-11-07 14:19:32 +0100

    ✨ Add an entry import file for the Math module
    Add an index.js file exporting all of the functions of Math module.

---
+++ src/Math/index.ts
@@ -0,0 +1,12 @@
+import add from "./add/add"
+import subtract from "./subtract/subtract"
+import multiply from "./multiply/multiply"
+import divide from "./divide/divide"
+
+
+export {
+    add,
+    subtract,
+    multiply,
+    divide
+}

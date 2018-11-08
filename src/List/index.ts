f6a41e45db79ec6555e97726872b038f0ab14ff0
TheByteBits (Mateusz Pietrzak) <mateusz.pietrzak@outlook.com>
2018-11-08 14:29:36 +0100

    ✨ Add a push function
    Add a push function with unit tests.

--- src/List/index.ts
+++ src/List/index.ts
@@ -1,8 +1,11 @@
 import reduce from "./reduce/reduce"
 import pop from "./pop/pop"
+import push from "./push/push"
 
 
 export {
     reduce,
+
+    push,
     pop
 }
---
+++ src/List/push/push.ts
@@ -0,0 +1,8 @@
+import { curry } from "../../Function"
+
+
+const push = (x, xs) =>
+    (xs.push(x), xs)
+
+
+export default curry(push)

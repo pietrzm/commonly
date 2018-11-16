93556dc20854dd1ff03a27abb28b16d1f32f1068
TheByteBits (Mateusz Pietrzak) <mateusz.pietrzak@outlook.com>
2018-11-16 16:33:14 +0100

    :spark: Add a compose function
    Add a compose function without unit tests.

---
+++ src/Function/compose/compose.ts
@@ -0,0 +1,8 @@
+import { reduce } from "../../List"
+
+
+const compose = (...fs) =>
+    reduce((accumulator, f) => (...vargs) => accumulator(f(...vargs)), x => x, fs)
+
+
+export default compose
--- src/Function/index.ts
+++ src/Function/index.ts
@@ -1,9 +1,12 @@
 import curry from "./curry/curry";
 import __ from "./__/__";
+import compose from "./compose/compose";
 
 
 export {
     curry,
-    __
+    __,
+
+    compose
 }
 

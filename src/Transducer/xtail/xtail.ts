44a8f50b5e61dae57a9a34a53d0e78ba8e3f2e6e
TheByteBits (Mateusz Pietrzak) <mateusz.pietrzak@outlook.com>
2018-11-29 15:08:04 +0100

    :sparkles: Add a xtail transducer
    Add a xtail transducer without unit tests.

---
+++ src/Transducer/xtail/xtail.ts
@@ -0,0 +1,11 @@
+import { curry } from "../../Function"
+import { xslice } from "../../Transducer"
+
+
+
+const xtail = (reducer) =>
+    xslice(1, Infinity, reducer)
+
+
+
+export default curry(xtail)

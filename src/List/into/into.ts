d71e210824d09a94f892240cbf5c874ae61132a9
TheByteBits <mateusz.pietrzak@outlook.com>
2018-12-09 19:40:08 +0100

    :hammer: Implement into function
    Implement into function in the most simple way.

--- src/List/into/into.ts
+++ src/List/into/into.ts
@@ -1,21 +1,34 @@
 import { curry } from "Function"
 import { isArray, isString, isSet, isMap } from "Type"
 import { transduce } from "List"
-import { cases, when } from "Logic"
 
 
 
-const into = (accumulator, xf, xs) =>
-    cases([
-        when(isArray,
-            () => transduce(xf, (xs, x) => (xs.push(x), xs), accumulator, xs)),
-        when(isString,
-            () => transduce(xf, (xs, x) => xs + x, accumulator, xs)),
-        when(isSet,
-            () => transduce(xf, (xs, x) => xs.add(x), accumulator, xs)),
-        when(isMap,
-            () => transduce(xf, (xs, x) => xs.set(...x), accumulator, xs)),
-    ], accumulator)
+const into = (accumulator, xf, xs) => {
+    switch (true) {
+        case isArray(accumulator):
+            return transduce(xf, (xs, x) => (xs.push(x), xs), accumulator, xs);
+        case isString(accumulator):
+            return transduce(xf, (xs, x) => xs + x, accumulator, xs);
+        case isSet(accumulator):
+            return transduce(xf, (xs, x) => xs.add(x), accumulator, xs);
+        case isMap(accumulator):
+            return transduce(xf, (xs, kx) => xs.set(kx[0], kx[1]), accumulator, xs);
+    }
+}
+
+
+// TODO: Rethink this implementation
+    // cases([
+    //     when(isArray,
+    //         () => transduce(xf, (xs, x) => (xs.push(x), xs), accumulator, xs)),
+    //     when(isString,
+    //         () => transduce(xf, (xs, x) => xs + x, accumulator, xs)),
+    //     when(isSet,
+    //         () => transduce(xf, (xs, x) => xs.add(x), accumulator, xs)),
+    //     when(isMap,
+    //         () => transduce(xf, (xs, x) => xs.set(...x), accumulator, xs))
+    // ], accumulator)
 
 
 

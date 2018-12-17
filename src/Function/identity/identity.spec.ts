e990b5846a92557a2e0e727a5f7794e430eb831b
TheByteBits (Mateusz Pietrzak) <mateusz.pietrzak@outlook.com>
2018-12-17 17:06:52 +0100

    :rotating_light: Add a spec for the identity function

---
+++ src/Function/identity/identity.spec.ts
@@ -0,0 +1,42 @@
+import identity from "Function/identity/identity"
+
+
+
+describe(`function identity(x)`, () => {
+	context(`no x`, () => {
+		it(`should return undefined`, () => {
+			expect((identity as any)())
+				.toBe(undefined)
+		})
+	})
+
+	context(`x is undefined`, () => {
+		it(`should return undefined`, () => {
+			expect(identity(undefined))
+				.toBe(undefined)
+		})
+	})
+
+
+	context(`x is null`, () => {
+		it(`should return null`, () => {
+			expect(identity(null))
+				.toBe(null)
+		})
+	})
+
+	context(`x is anything`, () => {
+		it(`should return passed in x`, () => {
+			const object = {},
+				array = [],
+				symbol = Symbol()
+
+			expect(identity(object))
+				.toBe(object)
+			expect(identity(array))
+				.toBe(array)
+			expect(identity(symbol))
+				.toBe(symbol)
+		})
+	})
+})

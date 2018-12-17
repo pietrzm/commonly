e09cf5909ffa34f6d3934477871bf6d47a6c6b37
TheByteBits (Mateusz Pietrzak) <mateusz.pietrzak@outlook.com>
2018-12-17 17:07:09 +0100

    :rotating_light: Add a spec for the isReduced function

---
+++ src/Function/isReduced/isReduced.spec.ts
@@ -0,0 +1,24 @@
+import isReduced from "Function/isReduced/isReduced"
+import reduced from "Function/reduced/reduced"
+
+
+
+describe(`function isReduced(x)`, () => {
+	context(`x is reduced`, () => {
+		it(`should return true`, () => {
+			const subject = reduced(Symbol())
+
+			expect(isReduced(subject))
+				.toBe(true)
+		})
+	})
+
+	context(`x is not reduced`, () => {
+		it(`should return false`, () => {
+			const subject = Symbol()
+
+			expect(isReduced(subject))
+				.toBe(false)
+		})
+	})
+})

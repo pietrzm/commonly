83088794f405dff8d6fd06b15f51859b43769d5d
TheByteBits (Mateusz Pietrzak) <mateusz.pietrzak@outlook.com>
2018-12-28 13:34:02 +0100

    :sparkles: Add a reducer function

---
+++ src/Function/reducer/reducer.spec.ts
@@ -0,0 +1,134 @@
+import reducer  from "Function/reducer/reducer"
+
+import identity from "Function/identity/identity"
+
+
+
+describe(`function reducer(xs)`, () => {
+	it(`should return a reducer.undefined function`, () => {
+		const subject = reducer(undefined)
+
+		expect(subject)
+			.toBe(reducer.undefined)
+	})
+
+	it(`should return a reducer.array function`, () => {
+		const subject = reducer([])
+
+		expect(subject)
+			.toBe(reducer.array)
+	})
+
+	it(`should return a reducer.string function`, () => {
+		const subject = reducer("")
+
+		expect(subject)
+			.toBe(reducer.string)
+	})
+
+	it(`should return a reducer.string function`, () => {
+		const subject = reducer(new Set())
+
+		expect(subject)
+			.toBe(reducer.set)
+	})
+
+	it(`should return a reducer.map function`, () => {
+		const subject = reducer(new Map())
+
+		expect(subject)
+			.toBe(reducer.map)
+	})
+})
+
+
+describe(`function reducer.undefined(accumulator, value)`, () => {
+	it(`should be an identity function`, () => {
+		const subject = reducer.undefined
+
+		expect(subject)
+			.toBe(identity)
+	})
+
+	it(`should be an undefined's reducer`, () => {
+		const subject = reducer.undefined(undefined)
+
+		expect(subject)
+			.toEqual(undefined)
+	})
+})
+
+
+describe(`function reducer.array(accumulator, value)`, () => {
+	it(`should be a reducer`, () => {
+		const subject = reducer.array
+
+		expect(subject.length)
+			.toEqual(2)
+	})
+
+	it(`should be an Array's reducer`, () => {
+		const subject = reducer.array([ 0, 1, 1, 2, 3, 5 ], 7)
+
+		expect(subject)
+			.toEqual([ 0, 1, 1, 2, 3, 5, 7 ])
+	})
+})
+
+
+describe(`function reducer.string(accumulator, value)`, () => {
+	it(`should be a reducer`, () => {
+		const subject = reducer.string
+
+		expect(subject.length)
+			.toEqual(2)
+	})
+
+	it(`should be a String's reducer`, () => {
+		const subject = reducer.string("011235", "7")
+
+		expect(subject)
+			.toEqual("0112357")
+	})
+})
+
+
+describe(`function reducer.set(accumulator, value)`, () => {
+	it(`should be a reducer`, () => {
+		const subject = reducer.set
+
+		expect(subject.length)
+			.toEqual(2)
+	})
+
+	it(`should be a Set's reducer`, () => {
+		const subject = reducer.set(new Set([ 0, 1, 1, 2, 3, 5 ]), 7)
+
+		expect(subject)
+			.toEqual(new Set([ 0, 1, 1, 2, 3, 5, 7 ]))
+	})
+})
+
+
+describe(`function reducer.map(accumulator, value)`, () => {
+	it(`should be a reducer`, () => {
+		const subject = reducer.map
+
+		expect(subject.length)
+			.toEqual(2)
+	})
+
+	it(`should be a Map's reducer`, () => {
+		const subject = reducer.map(new Map([
+			[ "A", 0 ], [ "B", 1 ], [ "C", 1 ],
+			[ "D", 2 ], [ "E", 3 ], [ "D", 5 ]
+		]), [ "E", 7 ])
+
+		expect(subject)
+			.toEqual(new Map([
+				[ "A", 0 ], [ "B", 1 ], [ "C", 1 ],
+				[ "D", 2 ], [ "E", 3 ], [ "D", 5 ],
+				[ "E", 7 ]
+			]))
+	})
+})
---
+++ src/Function/reducer/reducer.ts
@@ -0,0 +1,61 @@
+import Reducible   from "Protocol/Reducible"
+
+import identity    from "Function/identity/identity"
+
+import isUndefined from "Type/isUndefined/isUndefined"
+import isArray     from "Type/isArray/isArray"
+import isString    from "Type/isString/isString"
+import isMap       from "Type/isMap/isMap"
+import isSet       from "Type/isSet/isSet"
+
+
+
+/**
+ * Returns a reducing function for a given iterable.
+ *
+ * Function exposes explicitly following reducers:
+ * * reducer.undefined  - Reducer acting as identity function
+ * * reducer.array      - Reducer for Array's
+ * * reducer.string     - Reducer for String's
+ * * reducer.set        - Reducer for Set's
+ * * reducer.map        - Reducer for Map's
+ *
+ * @param {Iterable} xs
+ * @returns {(accumulator, value) => *}
+ */
+const reducer = (xs) => {
+	switch (true) {
+		case isUndefined(xs):
+			return reducer.undefined
+
+		case isArray(xs):
+			return reducer.array
+
+		case isString(xs):
+			return reducer.string
+
+		case isSet(xs):
+			return reducer.set
+
+		case isMap(xs):
+			return reducer.map
+
+		case Reducible.reducer in xs:
+			return xs[ Reducible.reducer ]
+	}
+}
+
+
+reducer.undefined = identity
+
+reducer.array = (xs, x) => (xs.push(x), xs)
+
+reducer.string = (xs, x) => xs + x
+
+reducer.map = (xs, kx) => xs.set(kx[0], kx[1])
+
+reducer.set = (xs, x) => xs.add(x)
+
+
+
+export default reducer

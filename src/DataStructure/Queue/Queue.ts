6e7ddfc48da5cad74d410411d2fd66f12aba6675
pguzek <guzekprzemyslaw@gmail.com>
2018-12-30 18:12:44 +0100

    redefined Queue<T> as interface-namespace via declaration merging

--- src/DataStructure/Queue/Queue.ts
+++ src/DataStructure/Queue/Queue.ts
@@ -1,15 +1,16 @@
 import identity from "Function/identity/identity";
 
-export default abstract class Queue<T> {
+interface Queue<T> {
+    enqueue(value: T): number
+    dequeue(): T
+    peek(): T
+    isEmpty(): boolean
+    size(): number
+    [Symbol.iterator](): Iterator<T>
+}
 
-    abstract enqueue(value: T): number
-    abstract dequeue(): T
-    abstract peek(): T
-    abstract isEmpty(): boolean
-    abstract size(): number
-    abstract [Symbol.iterator](): Iterator<T>
-
-    static from<K>(...values: K[]): Queue<K> {
+namespace Queue {
+    export function from<K>(...values: K[]): Queue<K> {
         const array = values.map(identity)
         return {
             enqueue: (value: K) => array.push(value),
@@ -21,3 +22,5 @@ export default abstract class Queue<T> {
         }
     }
 }
+
+export default Queue

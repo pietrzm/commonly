02ba04e97a4e4ea3f220dafee7781478eab78b76
TheByteBits (Mateusz Pietrzak) <mateusz.pietrzak@outlook.com>
2018-12-31 16:16:09 +0100

    :rotating_light: Fix broken test of a push function

--- src/Iterable/Array/push/push.spec.ts
+++ src/Iterable/Array/push/push.spec.ts
@@ -24,7 +24,7 @@ describe(`function pop(xs)`, () => {
 			const xs = [ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 ]
 
 			expect(push(55, xs))
-				.toEqual([ 0, 1, 1, 2, 3, 5, 8, 13, 21, 55 ])
+				.toEqual([ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 ])
 		})
 
 		it(`should mutate xs`, () => {

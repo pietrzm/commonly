3e87df379f4944ab0ed4d926922ce125ecd4025a
TheByteBits (Mateusz Pietrzak) <mateusz.pietrzak@outlook.com>
2019-01-11 16:14:52 +0100

    :sparkles: Add a xflatten transducer

---
+++ src/Transducer/xflatten/xflatten.ts
@@ -0,0 +1,11 @@
+import identity from "Function/identity/identity"
+import xchain from "Transducer/xchain/xchain"
+
+
+
+const xflatten = (xs) =>
+	xchain(identity, xs)
+
+
+
+export default xflatten

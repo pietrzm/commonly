2574a569aa1b95c73b8835d72b7bb954fbe6ecbe
pguzek <guzekprzemyslaw@gmail.com>
2019-01-15 11:42:52 +0100

    updated unit test descriptions

--- src/DataStructure/Stack/Stack.spec.ts
+++ src/DataStructure/Stack/Stack.spec.ts
@@ -2,28 +2,28 @@ import Stack from "./Stack"
 
 describe('interface Stack<T>', () => {
     context('empty stack', () => {
-        it('.push()', () => {
+        it('should add new elements at the top of the stack', () => {
             const q = Stack.from(...[] as number[])
             expect(q.push(2)).toEqual(1)
             expect(q.peek()).toEqual(2)
         })
-        it('.pop()', () => {
+        it('should remove element from the top of the stack', () => {
             const q = Stack.from(...[] as number[])
             expect(q.pop()).toBeUndefined()
         })
-        it('.peek()', () => {
+        it('should return the top element without removing it', () => {
             const q = Stack.from(...[] as number[])
             expect(q.peek()).toBeUndefined()
         })
-        it('.isEmpty()', () => {
+        it('should be empty', () => {
             const q = Stack.from(...[] as number[])
             expect(q.isEmpty()).toBeTruthy()
         })
-        it('.size()', () => {
+        it('should have zero elements', () => {
             const q = Stack.from(...[] as number[])
             expect(q.size()).toEqual(0)
         })
-        it('[Symbol.iterator]()', () => {
+        it('should be iterable with zero iteration steps', () => {
             const q = Stack.from(...[] as number[])
             let i = 0
             for(let x of q) {
@@ -33,28 +33,28 @@ describe('interface Stack<T>', () => {
         })
     })
     context('stack with one element', () => {
-        it('.push()', () => {
+        it('should add new elements at the top of the stack', () => {
             const q = Stack.from(10)
             expect(q.push(5)).toEqual(2)
             expect(q.peek()).toEqual(5)
         })
-        it('.pop()', () => {
+        it('should remove element from the top of the stack', () => {
             const q = Stack.from(10)
             expect(q.pop()).toEqual(10)
         })
-        it('.peek()', () => {
+        it('should return the top element without removing it', () => {
             const q = Stack.from(10)
             expect(q.peek()).toEqual(10)
         })
-        it('.isEmpty()', () => {
+        it('should not be empty', () => {
             const q = Stack.from(10)
             expect(q.isEmpty()).toBeFalsy()
         })
-        it('.size()', () => {
+        it('should have size of 1', () => {
             const q = Stack.from(10)
             expect(q.size()).toEqual(1)
         })
-        it('[Symbol.iterator]()', () => {
+        it('should be iterable with one iteration step', () => {
             const q = Stack.from(10)
             let i = 0
             for(let x of q) {
@@ -65,28 +65,28 @@ describe('interface Stack<T>', () => {
         })
     })
     context('stack with multiple elements', () => {
-        it('.push()', () => {
+        it('should add new elements at the top of the stack', () => {
             const q = Stack.from(1, 2, 3)
             expect(q.push(5)).toEqual(4)
             expect(q.peek()).toEqual(5)
         })
-        it('.pop()', () => {
+        it('should remove element from the top of the stack', () => {
             const q = Stack.from(1, 2, 3)
             expect(q.pop()).toEqual(3)
         })
-        it('.peek()', () => {
+        it('should return the top element without removing it', () => {
             const q = Stack.from(1, 2, 3)
             expect(q.peek()).toEqual(3)
         })
-        it('.isEmpty()', () => {
+        it('should not be empty', () => {
             const q = Stack.from(1, 2, 3)
             expect(q.isEmpty()).toBeFalsy()
         })
-        it('.size()', () => {
+        it('should have size equal to the number of elements', () => {
             const q = Stack.from(1, 2, 3)
             expect(q.size()).toEqual(3)
         })
-        it('[Symbol.iterator]()', () => {
+        it('should iterate over all of its elements', () => {
             const q = Stack.from(1, 2, 3)
             let i = 0
             let sum = 0

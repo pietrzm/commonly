7809a6741e91e5fe39db4d3aaec6f5aef9eb1ca9
TheByteBits (Mateusz Pietrzak) <mateusz.pietrzak@outlook.com>
2019-01-29 11:34:31 +0100

    :sparkles: Add a partial function

---
+++ src/Function/partial/partial.ts
@@ -0,0 +1,41 @@
+import size from "Iterable/size/size"
+
+
+
+const partial = (f, applied) => {
+	const df = (...varargs) => {
+		return f(...applied, ...varargs)
+	}
+
+	__copy_metadata(df, f, size(f) - size(applied))
+
+	return df
+}
+
+
+const __copy_metadata = (df, f, n) => {
+	Object.defineProperties(df, {
+		name: {
+			value: f.name,
+			configurable: true,
+			enumerable: false,
+			writable: false
+		},
+		length: {
+			value: n,
+			configurable: true,
+			enumerable: false,
+			writable: false
+		},
+		toString: {
+			value: () => f.toString(),
+			configurable: true,
+			enumerable: false,
+			writable: true
+		},
+	})
+}
+
+
+
+export default partial

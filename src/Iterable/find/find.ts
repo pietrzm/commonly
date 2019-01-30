bc5bd85a9d1131f35e5f99c7d92659ac7519c02f
TheByteBits (Mateusz Pietrzak) <mateusz.pietrzak@outlook.com>
2019-01-30 16:15:48 +0100

    :barber: Add static typing for the find function

--- src/Iterable/find/find.ts
+++ src/Iterable/find/find.ts
@@ -1,12 +1,15 @@
 import autocurry from "Function/autocurry/autocurry"
 import into  from "Iterable/into/into"
 import xfind from "Transducer/xfind/xfind"
+import Predicate from "Type/Predicate/Predicate"
 
 
 
-const find = (predicate, xs) =>
+const find = <T>(predicate: Predicate<T>, xs: Iterable<T>): T | undefined =>
     into(undefined, xfind(predicate), xs)
 
 
 
-export default autocurry(find)
+export default autocurry(find) as {
+    <T>(transformer: Predicate<T>, xs: Iterable<T>): T | undefined
+}
